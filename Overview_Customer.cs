0efc50188f22245cc9dbc01d561f52a23787e42b
AiCon3R <1054255@hr.nl>
2023-05-16 17:59:59 +0200

    balls pt.2

--- Overview_Customer.cs
+++ Overview_Customer.cs
@@ -1,183 +1,54 @@
 ﻿using System;
-using System.IO;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-// using System.Text.Json;
+using System.Text.Json;
 
-class Overzicht_Customer
+public static class Overzicht_Customer
 {
-    // static public void User(bool user)
-    // {
-    //     // string jsondata = File.ReadAllText("MovieBio.json");
-    //     // var movie = JsonSerializer.Deserialize<Movies[]>(jsondata);
-    //     // Console.WriteLine("Overview Current Movies\n");
-    //     Console.WriteLine("Druk [A] om het huidg overzicht van de films te zien.");
-    //     Console.WriteLine("Druk [B] om terug naar het menu te gaan.");
-    //     string input0 = Console.ReadLine().ToLower();
-
-    //     if (input0 == "a")
-    //     {
-    //         List<Movies> movies = GetMovieData();
-    //         ViewData(movies);
-    //         Console.WriteLine("Druk [A] om meer informatie te lezen over een van de films.");
-    //         Console.WriteLine("Druk [B] om terug naar het menu te gaan.");
-    //         string input1 = Console.ReadLine().ToLower();
-    //         if (input1 == "a")
-    //         {
-    //             Console.WriteLine("Typ een film in:\n");
-    //             string input2 = Console.ReadLine().ToLower();
-    //         }
-    //     }
-    //     else if (input0 == "b")
-    //     {
-    //         Console.Clear();
-    //         Console.WriteLine("Terug gaan naar het menu...");
-    //         Menu.Start(user);
-    //     }
-    //     else
-    //     {
-    //         Console.WriteLine("Ongeldige invoer. Probeer het opnieuw.\n");
-    //         User(user);
-    //     }
-    // }
-
     static public void User(bool user)
     {
-        Console.WriteLine("Druk [A] om het huidige overzicht van de films te zien.");
-        Console.WriteLine("Druk [B] om terug naar het menu te gaan.");
-        string input0 = Console.ReadLine().ToLower();
+        Console.WriteLine("Overzicht van huidige films:\n");
 
-        if (input0 == "a")
+        string jsondata = File.ReadAllText("MovieBio.json");
+        var movies = JsonSerializer.Deserialize<Movies[]>(jsondata);
+
+        for (int i = 0; i < movies.Length; i++)
         {
-            List<Movies> movies = GetMovieData();
-            ViewData(movies);
-            Console.WriteLine("Typ een zoekterm in:\n");
-            string searchQuery = Console.ReadLine().ToLower();
-            List<Movies> searchResults = SearchMovies(movies, searchQuery);
+            Console.WriteLine($"[{i + 1}] {movies[i].Title}");
+        }
 
-            if (searchResults.Count > 0)
+        Console.WriteLine("[T] Terug naar het menu");
+        Console.WriteLine("Selecteer een film:");
+
+        string? input0 = Console.ReadLine();
+        if (input0?.ToLower() == "t")
+        {
+            Menu.Start(user);
+        }
+
+        if (int.TryParse(input0, out int selectedIndex) && selectedIndex >= 1 && selectedIndex <= movies.Length)
+        {
+            PrintMov(movies[selectedIndex - 1]);
+
+            Console.WriteLine("[S] Selecteer een andere film");
+            Console.WriteLine("[T] Terug naar het menu");
+
+            string input = Console.ReadLine();
+            if (input?.ToLower() == "s")
             {
-                Console.WriteLine("Zoekresultaten:\n");
-                ViewData(searchResults);
-                Console.WriteLine("Druk [C] om meer informatie te lezen over een van de films.");
-                Console.WriteLine("Druk [B] om terug naar het menu te gaan.");
-                string input1 = Console.ReadLine().ToLower();
-
-                if (input1 == "c")
-                {
-                    Console.WriteLine("Typ de titel van de film om meer informatie te lezen:\n");
-                    string movieTitle = Console.ReadLine().ToLower();
-                    Movies selectedMovie = searchResults.FirstOrDefault(movie => movie.Title.ToLower() == movieTitle);
-
-                    if (selectedMovie != null)
-                    {
-                        PrintMov2(selectedMovie);
-                        Console.WriteLine("Druk [B] om terug naar het menu te gaan.");
-                        string input2 = Console.ReadLine().ToLower();
-
-                        if (input2 == "b")
-                        {
-                            Console.Clear();
-                            Console.WriteLine("Terug gaan naar het menu...");
-                            Menu.Start(user);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Film niet gevonden.");
-                        User(user);
-                    }
-                }
-                else if (input1 == "b")
-                {
-                    Console.Clear();
-                    Console.WriteLine("Terug gaan naar het menu...");
-                    Menu.Start(user);
-                }
-                else
-                {
-                    Console.WriteLine("Ongeldige invoer. Probeer het opnieuw.\n");
-                    User(user);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Geen zoekresultaten gevonden.");
                 User(user);
             }
-        }
-        else if (input0 == "b")
-        {
-            Console.Clear();
-            Console.WriteLine("Terug gaan naar het menu...");
-            Menu.Start(user);
+            else if (input?.ToLower() == "t")
+            {
+                Menu.Start(user);
+            }
         }
         else
         {
-            Console.WriteLine("Ongeldige invoer. Probeer het opnieuw.\n");
+            Console.WriteLine("Ongeldige invoer");
             User(user);
         }
     }
 
-    static List<Movies> SearchMovies(List<Movies> movies, string searchQuery)
-    {
-        return movies.Where(movie =>
-            movie.Title.ToLower().Contains(searchQuery) ||
-            movie.Genres.ToLower().Contains(searchQuery) ||
-            movie.Director.ToLower().Contains(searchQuery) ||
-            movie.LeadActor.ToLower().Contains(searchQuery)
-        ).ToList();
-    }
-
-
-    static List<Movies> GetMovieData()
-    {
-        string jsonData = File.ReadAllText("MovieBio.json");
-        Newtonsoft.Json.Linq.JArray data = JArray.Parse(jsonData);
-        List<Movies> movies = new List<Movies>();
-        foreach (JObject item in data)
-        {
-            Movies movie = new Movies
-            {
-                Title = (string?)item["Title"],
-                Genres = (string?)item["Genres"],
-                Director = (string?)item["Director"],
-                LeadActor = (string?)item["LeadActor"],
-                Duration = (int)item["Duration"],
-                Release = (string?)item["Release"],
-                Description = (string?)item["Description"],
-                Price = (string?)item["Price"]
-            };
-            movies.Add(movie);
-        }
-        return movies;
-    }
-
-    static void ViewData(List<Movies> movies)
-    {
-        Console.WriteLine("Overzicht van huidige films:\n");
-        foreach (Movies movie in movies)
-        {
-            Console.WriteLine($"Title: {movie.Title}");
-            Console.WriteLine($"Genres: {movie.Genres}");
-            // Console.WriteLine($"Director(s): {movie.Director}");
-            // Console.WriteLine($"Lead actor(s): {movie.LeadActor}");
-            Console.WriteLine($"Duration: {movie.Duration} minutes");
-            // Console.WriteLine($"Release date: {movie.Release}");
-            // Console.WriteLine($"Description: {movie.Description}");
-            Console.WriteLine($"Price: {movie.Price},-\n");
-        }
-    }
-
-    // static void PrintMov1(Movies movie)
-    //     {
-    //         Console.WriteLine($"Titel: {movie.Title}");
-    //         Console.WriteLine($"Genres: {movie.Genres}");
-    //         Console.WriteLine($"Duur: {movie.Duration} minuten");
-    //         Console.WriteLine($"Standaard prijs(2D): {movie.Price} euro");
-    //     }
-
-    static void PrintMov2(Movies movie)
+    static void PrintMov(Movies movie)
     {
         Console.WriteLine($"Titel: {movie.Title}");
         Console.WriteLine($"Genres: {movie.Genres}");
@@ -188,5 +59,4 @@ class Overzicht_Customer
         Console.WriteLine($"Beschrijving: {movie.Description}");
         Console.WriteLine($"Standaard prijs(2D): {movie.Price} euro");
     }
-
 }

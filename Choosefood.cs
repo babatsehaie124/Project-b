15245ed9b5d7a4c3b873300c683c0ed9cb39142f
Rushillachmon <125268172+Rushillachmon@users.noreply.github.com>
2023-06-07 14:32:43 +0200

    r

--- Choosefood.cs
+++ Choosefood.cs
@@ -1,23 +1,16 @@
-public class Choosefood
-{
-    public static void PickFood()
-    {
-        Console.WriteLine("Bedankt voor het reserveren van een ticket");
-        Console.WriteLine("Wilt u?:");
-        Console.WriteLine("[E] Eten en drinken nu bestellen");
-        Console.WriteLine("[D] Doorgaan naar de laatste stap");
-        string? pick = Console.ReadLine().ToLower();
-        if (pick == "D")
-        {
-            Console.WriteLine("U wordt doorgestuurd naar u bon");
-        }
-        if (pick == "E")
-        {
 
-        }
 
-        if (pick == "T")
 
-            Console.WriteLine("Welkom bij onze Eet en drinkmenu");
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+

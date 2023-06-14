5d83bd9f98752b92e4f3ad7179741bfa0ad274d0
Jay1317 <125268292+Jay1317@users.noreply.github.com>
2023-06-14 13:38:42 +0200

    Update Schedule.cs

--- Schedule.cs
+++ Schedule.cs
@@ -93,7 +93,7 @@ ______ _                                  ______      _   _               _
         while (!exit)
         {
             Console.Clear();
-            Console.WriteLine("Voer een dag in (Maandag, Dinsdag, Woensdag, etc.");
+            Console.WriteLine("Voer een dag in (Maandag, Dinsdag, Woensdag, etc.)");
             Console.WriteLine("[T] - Terug naar het roostermenu");
             string input = Console.ReadLine();
             input = Char.ToUpper(input[0]) + input.Substring(1);
@@ -105,10 +105,12 @@ ______ _                                  ______      _   _               _
             }
             else if (scheduleByDay.ContainsKey(input))
             {
-                Console.Clear();
                 List<MovieSchedule> movieSchedules = scheduleByDay[input];
 
                 Console.WriteLine(input);
+
+                // Sort movie schedules by start time
+                movieSchedules.Sort((a, b) => DateTime.Parse(a.Start).CompareTo(DateTime.Parse(b.Start)));
 
                 foreach (var movieSchedule in movieSchedules)
                 {
@@ -179,7 +181,6 @@ ______ _                                  ______      _   _               _
         bool exit = false;
         while (!exit)
         {
-            Console.Clear();
             Console.WriteLine("Voer een dag in (Maandag, Dinsdag, Woensdag, etc.)");
             Console.WriteLine("[T] - Terug naar het roostermenu");
             string input = Console.ReadLine();
@@ -197,8 +198,6 @@ ______ _                                  ______      _   _               _
                 Console.Clear();
 
                 List<MovieSchedule> movieSchedules = scheduleByDay[input];
-
-                Console.Clear();
 
                 Console.WriteLine(input);
 

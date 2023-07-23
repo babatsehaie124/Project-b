9222146e36f52acbac61761b9bc689fee9a9e372
Rushillachmon <125268172+Rushillachmon@users.noreply.github.com>
2023-07-23 18:13:33 +0200

    etenswaren bekijken vanuit adminpanel

--- Overview_Admin.cs
+++ Overview_Admin.cs
@@ -28,7 +28,9 @@ ______ _                                  ______      _   _               _
         Console.WriteLine("[D] - Verwijder een film");
         Console.WriteLine("[E] - Vragen van klanten bekijken");
         Console.WriteLine("[F] - Klantgegevens bekijken");
-        Console.WriteLine("[G] - Terugkeren naar het menu");
+        Console.WriteLine("[G] - Overzicht van huidige etenswaren");
+        Console.WriteLine("[H] - Voeg een etenswaar toe");
+        Console.WriteLine("[I] - Terugkeren naar het menu");
 
         // optie voor rooster zien en kunnen wijzigen
 
@@ -73,6 +75,10 @@ ______ _                                  ______      _   _               _
         }
         else if (input == "G")
         {
+            ViewFood(user);
+        }
+        else if (input == "I")
+        {
             Console.Clear();
             Console.WriteLine("U wordt teruggestuurd naar het menu...");
             Thread.Sleep(3000); // gamechanger
@@ -81,7 +87,6 @@ ______ _                                  ______      _   _               _
             user = false;
             Menu.Start(user);
         }
-
         else
         {
             Console.WriteLine("Onjuiste keuze. Probeer nogmaals.\n");
@@ -381,6 +386,55 @@ ______ _                                  ______      _   _               _
         }
         Console.ResetColor();
         return usergegevens;
+    }
+
+    static public void ViewFood(bool user)
+    {
+        string menu2 = @"
+______ _                                  ______      _   _               _                 
+| ___ (_)                                 | ___ \    | | | |             | |                
+| |_/ /_  ___  ___  ___ ___   ___  _ __   | |_/ /___ | |_| |_ ___ _ __ __| | __ _ _ __ ___  
+| ___ | |/ _ \/ __|/ __/ _ \ / _ \| '_ \  |    // _ \| __| __/ _ | '__/ _` |/ _` | '_ ` _ \ 
+| |_/ | | (_) \__ | (_| (_) | (_) | |_) | | |\ | (_) | |_| ||  __| | | (_| | (_| | | | | | |
+\____/|_|\___/|___/\___\___/ \___/| .__/  \_| \_\___/ \__|\__\___|_|  \__,_|\__,_|_| |_| |_|
+                                  | |                                                       
+                                  |_|";
+
+        Console.WriteLine(menu2);
+        Console.WriteLine();
+        Console.Clear();
+
+
+        Console.WriteLine("Welkom bij ons eet-drinkmenu");
+        Console.WriteLine("Als u iets vanuit dit menu wilt bestellen");
+        Console.WriteLine("Ga terug naar het Menu -> Reserveren -> Eten bestellen\n");
+        string storedata = File.ReadAllText("Store.json");
+        var products = JsonConvert.DeserializeObject<List<Storeproducts>>(storedata);
+        Console.Clear();
+
+        foreach (var product in products)
+        {
+            Console.WriteLine("                                   Extra informatie:");
+            Console.WriteLine($"Kleine Popcorn: {product.Small_Popcorn} euro           | Dezelfde prijzen gelden voor de smaken: Zoet, Zout, Mix en Caramel.");
+            Console.WriteLine($"Medium Popcorn: {product.Medium_Popcorn} euro");
+            Console.WriteLine($"Grote Popcorn:  {product.Large_Popcorn} euro\n");
+
+            Console.WriteLine($"Nachos:         {product.Nachos} euro           | Dit zijn driehoekvormige chips met een lichte paprika smaak.");
+            Console.WriteLine($"Loaded Nachos:  {product.Loaded_Nachos} euro         | Bevat Paprika saus, uien, augurken, jalapeños en kaas.\n");
+
+            Console.WriteLine($"Hotdog:         {product.Hotdog} euro        | Bevat varkensvlees en saus is naar keuze.");
+            Console.WriteLine($"Loaded Hotdog:  {product.Loaded_Hotdog} euro         | Bevat Varkensvlees, Mosterd saus, kaas, uien en paprika stukjes.\n");
+
+            Console.WriteLine($"Koude dranken:  {product.Cold_Drinks} euro         | Dezelfde prijzen voor alle dranken: Cola, Chaudfontaine blauw (water), Fristy, Chocolademelk, Lipton, Fanta, Sprite en 7up.");
+            Console.WriteLine($"Warme dranken:  {product.Hot_Drinks} euro         | Dezelfde prijzen voor alle dranken: Alle soorten koffie, warme chocolademelk, warme water.\n");
+
+            Console.WriteLine($"Kindermenu:     {product.Kids_Meal} euro         | Bevat Kleine stukjes chocolade, een speelgoed, en een appelmoes.\n");
+
+            Console.WriteLine($"Snoepzak:       {product.Candy_Bag} euro           | Bevat stukjes van Haribo, rode drop, Pico Bella, kleine lolly, Maoam.\n");
+
+            Console.WriteLine($"Chips:          {product.Crisps} euro           | Bevat alleen Lays merk chips.\n");
+        }
+        OngInvoer(user);
     }
 
     public static void OngInvoer(bool user)
